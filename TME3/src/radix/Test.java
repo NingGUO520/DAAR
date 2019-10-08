4df77f7e9ec64b58b2a8b600d6a440fb602b128c
Ning GUO <ai535463858@gmail.com>
2019-10-08 23:35:36 +0200

    update tme3

--- TME3/src/radix/Test.java
+++ TME3/src/radix/Test.java
@@ -1,5 +1,8 @@
 package radix;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -8,10 +11,13 @@ public class Test {
 
 	public class RadixTree{
 
-
-
 		String chars;
 		ArrayList<RadixTree> fils ;
+		public RadixTree() {
+			chars = null;
+			fils = new ArrayList<RadixTree>();
+		}
+		
 		public RadixTree(String lettres) {
 			chars = lettres;
 			fils = new ArrayList<RadixTree>();
@@ -27,9 +33,8 @@ public class Test {
 			return fils.isEmpty();
 
 		}
-		
-
 	}
+
 	public class Pair{
 		int ligne;
 		int col;
@@ -39,6 +44,7 @@ public class Test {
 		}
 
 	}
+
 	public class Coordonnees{
 		ArrayList<Pair> coords;
 
@@ -73,6 +79,7 @@ public class Test {
 
 		return true;
 	}
+
 	public void insertion(RadixTree arbre, String mot) {
 		if(arbre.est_feuille()) {
 			if(!mot.isEmpty()) {
@@ -80,14 +87,16 @@ public class Test {
 				arbre.fils.add(nouvelle);
 			}
 		}else {
-			for(RadixTree fil:arbre.fils) {
+			for(int i = 0; i< arbre.fils.size();i++) {
+				RadixTree fil = arbre.fils.get(i);
 				if(estPrefix(mot,fil.chars)){
-					 insertion(fil,mot.substring(fil.chars.length()));
+					insertion(fil,mot.substring(fil.chars.length()));
+				}else {
+					RadixTree nouvelle = new RadixTree(mot);
+					arbre.fils.add(nouvelle);
 				}
 			}
 		}
-		
-
 	}
 
 	public boolean rechercher(RadixTree arbre, String motif) {
@@ -103,61 +112,84 @@ public class Test {
 			}
 
 		}
-
 		return false;
-
 	}
-	public void lireTexte() {
-		HashMap<String,Coordonnees> mapIndex = new HashMap<String,Coordonnees>();
 
-		String s = "bonjour, je m'appelle Jean-jacque , bonjour";
-		System.out.println("substring"+s.substring(3));
-		int i = 0;
-		String mot ="";
-		int debut = 0;
-		while(i<s.length()) {
-
-
-			char c = s.charAt(i);
-			int n = (int)c;
-			if((n>=97 && n<=122)
-					|| (n>=65 && n<=90 ) || n == 39 || n == 45) {
-				mot+=c;
-				if(i == s.length()-1) {
-					if(mapIndex.containsKey(mot)) {
-						mapIndex.get(mot).addCoord(1, debut);
-					}else {
-						Coordonnees coord = new Coordonnees(1,debut);
-						mapIndex.put(mot,coord );
-					}
-				}
-
-
-			}else {
-				if(mot.length()>0 ) {
-					if(mapIndex.containsKey(mot)) {
-						mapIndex.get(mot).addCoord(1, debut);
-					}else {
-						Coordonnees coord = new Coordonnees(1,debut);
-						mapIndex.put(mot,coord );
-						mot ="";
-					}
-
-				}
-				debut = i+1;
-			}
-			i++;
-
+	public RadixTree construireTree(HashMap<String,Coordonnees> mapIndex) {
+		RadixTree tree = new RadixTree();
+		for(Entry<String,Coordonnees> e :mapIndex.entrySet()) {
+			String mot = e.getKey();
+			insertion(tree,mot);
 		}
 
+		return tree;
+	}
+	
+	public HashMap<String,Coordonnees> lireTexte(String nomFichier) {
+
+		HashMap<String,Coordonnees> mapIndex = new HashMap<String,Coordonnees>();
+		ArrayList<String> lignes = new ArrayList<String>();
+		String ligne;
+		BufferedReader reader;
+
+		try {
+			reader = new BufferedReader(new FileReader(nomFichier));
+			while ((ligne = reader.readLine()) != null) {
+				lignes.add(ligne);
+			}
+			reader.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		for(int i = 0 ;i< lignes.size();i++) {
+			String s = lignes.get(i);
+			int j = 0;
+			String mot ="";
+			int debut = 1;
+			while(j<s.length()) {
+				char c = s.charAt(j);
+				int n = (int)c;
+				if((n>=97 && n<=122) || (n>=224 && n<=252)
+						|| (n>=65 && n<=90 ) || n == 39 || n == 45) {
+					mot+=c;
+					if(j == s.length()-1) {
+						if(mapIndex.containsKey(mot)) {
+							mapIndex.get(mot).addCoord(i+1, debut);
+						}else {
+							Coordonnees coord = new Coordonnees(i+1,debut);
+							mapIndex.put(mot,coord );
+						}
+						mot ="";
+					}
+				}else {
+					if(mot.length()>0 ) {
+						if(mapIndex.containsKey(mot)) {
+							mapIndex.get(mot).addCoord(i+1, debut);
+						}else {
+							Coordonnees coord = new Coordonnees(i+1,debut);
+							mapIndex.put(mot,coord );
+						}
+						mot ="";
+						debut = j+2;
+					}
+
+				}
+				j++;
+			}
+		}
+		return mapIndex;
+	}
+	
+	public static void main(String[] args) {
+		Test t = new Test();
+
+		HashMap<String,Coordonnees> mapIndex = t.lireTexte("test.txt");
 		for(Entry<String,Coordonnees> e :mapIndex.entrySet()) {
 
 			System.out.println(e.getKey()+e.getValue().toString());
 		}
-	}
-	public static void main(String[] args) {
-		Test t = new Test();
-		t.lireTexte();
+		RadixTree tree = t.construireTree(mapIndex);
 
 
 	}
